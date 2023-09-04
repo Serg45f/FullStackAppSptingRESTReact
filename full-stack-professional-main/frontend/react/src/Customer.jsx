1cd0f30b1da5fca7e8253acb8d31ed202cc05ff7
Serhii_Savchenko <sgsavch75@gmail.com>
2023-09-04 19:50:41 +0300

    Added frontend books

--- full-stack-professional-main/frontend/react/src/Customer.jsx
+++ full-stack-professional-main/frontend/react/src/Customer.jsx
@@ -1,95 +1,83 @@
-import {
-    Wrap,
-    WrapItem,
-    Spinner,
-    Text
-} from '@chakra-ui/react';
-import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import { Spinner, Text, Wrap, WrapItem } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { getCustomers } from "./services/client.js";
-import CardWithImage from "./components/customer/CustomerCard.jsx";
-import CreateCustomerDrawer from "./components/customer/CreateCustomerDrawer.jsx";
-import {errorNotification} from "./services/notification.js";
+import CreateCustomerDrawer from './components/customer/CreateCustomerDrawer.jsx';
+import CardWithCustomerImage from './components/customer/CustomerCard.jsx';
+import SidebarWithHeader from './components/shared/SideBar.jsx';
+import { getCustomers } from './services/client.js';
+import { errorNotification } from './services/notification.js';
 
 const Customer = () => {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [err, setError] = useState('');
 
-    const [customers, setCustomers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [err, setError] = useState("");
+  const fetchCustomers = () => {
+    setLoading(true);
+    getCustomers()
+      .then((res) => {
+        setCustomers(res.data);
+      })
+      .catch((err) => {
+        setError(err.response.data.message);
+        errorNotification(err.code, err.response.data.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
-    const fetchCustomers = () => {
-        setLoading(true);
-        getCustomers().then(res => {
-            setCustomers(res.data)
-        }).catch(err => {
-            setError(err.response.data.message)
-            errorNotification(
-                err.code,
-                err.response.data.message
-            )
-        }).finally(() => {
-            setLoading(false)
-        })
-    }
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
 
-    useEffect(() => {
-        fetchCustomers();
-    }, [])
-
-    if (loading) {
-        return (
-            <SidebarWithHeader>
-                <Spinner
-                    thickness='4px'
-                    speed='0.65s'
-                    emptyColor='gray.200'
-                    color='blue.500'
-                    size='xl'
-                />
-            </SidebarWithHeader>
-        )
-    }
-
-    if (err) {
-        return (
-            <SidebarWithHeader>
-                <CreateCustomerDrawer
-                    fetchCustomers={fetchCustomers}
-                />
-                <Text mt={5}>Ooops there was an error</Text>
-            </SidebarWithHeader>
-        )
-    }
-
-    if(customers.length <= 0) {
-        return (
-            <SidebarWithHeader>
-                <CreateCustomerDrawer
-                    fetchCustomers={fetchCustomers}
-                />
-                <Text mt={5}>No customers available</Text>
-            </SidebarWithHeader>
-        )
-    }
-
+  if (loading) {
     return (
-        <SidebarWithHeader>
-            <CreateCustomerDrawer
-                fetchCustomers={fetchCustomers}
+      <SidebarWithHeader>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      </SidebarWithHeader>
+    );
+  }
+
+  if (err) {
+    return (
+      <SidebarWithHeader>
+        <CreateCustomerDrawer fetchCustomers={fetchCustomers} />
+        <Text mt={5}>Ooops there was an error</Text>
+      </SidebarWithHeader>
+    );
+  }
+
+  if (customers.length <= 0) {
+    return (
+      <SidebarWithHeader>
+        <CreateCustomerDrawer fetchCustomers={fetchCustomers} />
+        <Text mt={5}>No customers available</Text>
+      </SidebarWithHeader>
+    );
+  }
+
+  return (
+    <SidebarWithHeader>
+      <CreateCustomerDrawer fetchCustomers={fetchCustomers} />
+      <Wrap justify={'center'} spacing={'30px'}>
+        {customers.map((customer, index) => (
+          <WrapItem key={index}>
+            <CardWithCustomerImage
+              {...customer}
+              imageNumber={index}
+              fetchCustomers={fetchCustomers}
             />
-            <Wrap justify={"center"} spacing={"30px"}>
-                {customers.map((customer, index) => (
-                    <WrapItem key={index}>
-                        <CardWithImage
-                            {...customer}
-                            imageNumber={index}
-                            fetchCustomers={fetchCustomers}
-                        />
-                    </WrapItem>
-                ))}
-            </Wrap>
-        </SidebarWithHeader>
-    )
-}
+          </WrapItem>
+        ))}
+      </Wrap>
+    </SidebarWithHeader>
+  );
+};
 
 export default Customer;
--- full-stack-professional-main/frontend/react/src/main.jsx
+++ full-stack-professional-main/frontend/react/src/main.jsx
@@ -1,46 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Customer from './Customer.jsx'
-import {ChakraProvider, Text} from '@chakra-ui/react'
-import { createStandaloneToast } from '@chakra-ui/toast'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./components/login/Login.jsx";
-import Signup from "./components/signup/Signup";
-import AuthProvider from "./components/context/AuthContext.jsx";
-import ProtectedRoute from "./components/shared/ProtectedRoute.jsx";
-import './index.css'
-import Home from "./Home.jsx";
+import { ChakraProvider } from '@chakra-ui/react';
+import { createStandaloneToast } from '@chakra-ui/toast';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Book from './Book.jsx';
+import Customer from './Customer.jsx';
+import Home from './Home.jsx';
+import AuthProvider from './components/context/AuthContext.jsx';
+import Login from './components/login/Login.jsx';
+import ProtectedRoute from './components/shared/ProtectedRoute.jsx';
+import Signup from './components/signup/Signup';
+import './index.css';
 
 const { ToastContainer } = createStandaloneToast();
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login />
-    },
-    {
-        path: "/signup",
-        element: <Signup />
-    },
-    {
-        path: "dashboard",
-        element: <ProtectedRoute><Home/></ProtectedRoute>
-    },
-    {
-        path: "dashboard/customers",
-        element: <ProtectedRoute><Customer /></ProtectedRoute>
-    }
-])
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: 'dashboard',
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: 'dashboard/customers',
+    element: (
+      <ProtectedRoute>
+        <Customer />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: 'dashboard/books',
+    element: (
+      <ProtectedRoute>
+        <Book />
+      </ProtectedRoute>
+    ),
+  },
+]);
 
-ReactDOM
-    .createRoot(document.getElementById('root'))
-    .render(
-        <React.StrictMode>
-            <ChakraProvider>
-                <AuthProvider>
-                    <RouterProvider router={router} />
-                </AuthProvider>
-                <ToastContainer />
-            </ChakraProvider>
-        </React.StrictMode>,
-    )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      <ToastContainer />
+    </ChakraProvider>
+  </React.StrictMode>
+);
